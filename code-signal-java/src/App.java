f46432637e058239b217843648e882d1f25dffec
moysescosta <mecs.cwb@gmail.com>
2024-12-30 16:14:23 -0300

    #

--- code-signal-java/src/App.java
+++ code-signal-java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numberOfStars = 88; //criando e declarando o valor da variável.
+        System.out.println(numberOfStars); //Por fim, verificamos seu conteúdo. Saída: 88
     }
 }
