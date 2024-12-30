550c9bae3d7702465995884879d655a623bf1fa9
moysescosta <mecs.cwb@gmail.com>
2024-12-30 16:31:20 -0300

    #

--- code-signal-java/src/condeSignal/atribuicaoJava/atribuicao.java
+++ code-signal-java/src/condeSignal/atribuicaoJava/atribuicao.java
@@ -1,10 +1,15 @@
 package condeSignal.atribuicaoJava;
 
 public class atribuicao {
-
+    public static void main(String[] args){
     int constellations = 88; // Foi criada uma caixa (variável), nomeada (constellations) e depositado um valor.
     System.out.println(constellations);
 
     constellations = 77; // Aqui foi alterado o valor da variável.
     System.out.println(constellations); // Verificado a atualização do conteúdo. Nova saída: 77.
 }
+}
+
+/*##Operações de Aribuição em Java
+Atribuição em Java envolve alocar ou alterar um valor para uma variável usando o operador =. Esse processo se assemelha a colocar um item em uma caixa.
+ */
